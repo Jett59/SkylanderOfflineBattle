4b8991e378bd69bfb190f33eb1c8eea7d96b201d
Luke Thompson <lthompson@infomedia.com.au>
2015-08-02 20:17:13 +1000

    Add player list and persistence of preferences.

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,5 +1,13 @@
+import com.sun.java.swing.ui.CommonUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Vector;
+import java.util.prefs.Preferences;
 
 /**
  * User: luke
@@ -7,8 +15,51 @@ import java.util.Arrays;
  * Time: 6:50 AM
  */
 public class EntryPoint {
+    static Preferences prefs = Preferences.userNodeForPackage(EntryPoint.class);
+
     public static void main(String[] args) throws IOException {
-        DoSomething doSomething = new DoSomething();
-        doSomething.execute(Arrays.asList("test123", "test2", "test31238"));
+
+
+        final JFrame battleWindow = new JFrame("Skylander Offline Battle!");
+        battleWindow.setSize(new Dimension(640, 480));
+        battleWindow.setBackground(Color.BLACK);
+
+        final Box verticalBox = Box.createVerticalBox();
+        addPlayerList(battleWindow, verticalBox);
+
+        battleWindow.add(verticalBox);
+
+        battleWindow.setVisible(true);
+    }
+
+    private static void addPlayerList(final JFrame battleWindow, Box verticalBox) {
+
+        final Vector<String> playerList = new Vector<String>(Arrays.asList(prefs.get("players", "").split(",")));
+
+        final JList playerListComponent = new JList(playerList);
+
+        verticalBox.add(CommonUI.createListPane(playerListComponent, "Players"));
+
+        verticalBox.add(CommonUI.createButton("Add Player", new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String result = JOptionPane.showInputDialog(
+                        battleWindow,
+                        "Enter player name",
+                        "Create New Player",
+                        JOptionPane.PLAIN_MESSAGE);
+
+                playerList.add(result);
+                playerListComponent.updateUI();
+
+                StringBuilder players = new StringBuilder();
+                for (String player : playerList) {
+                    if (players.length()>0) {
+                        players.append(",");
+                    }
+                    players.append(player);
+                }
+                prefs.put("players", players.toString());
+            }
+        }));
     }
 }
